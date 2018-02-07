321281b1ebe1c3a7593c570684d7803645517ab9
bdclark <brian@clark.zone>
2018-02-07 16:54:13 -0500

    nomadctl version v0.0.3

--- Formula/nomadctl.rb
+++ Formula/nomadctl.rb
@@ -1,9 +1,9 @@
 class Nomadctl < Formula
   desc ""
   homepage "https://github.com/bdclark/nomadctl"
-  url "https://github.com/bdclark/nomadctl/releases/download/v0.0.2/nomadctl_0.0.2_darwin_amd64.tar.gz"
-  version "0.0.2"
-  sha256 "f0a86b7d57408e185e1bac9c84f10825d26dcef856292f748dda0e22d8308917"
+  url "https://github.com/bdclark/nomadctl/releases/download/v0.0.3/nomadctl_0.0.3_darwin_amd64.tar.gz"
+  version "0.0.3"
+  sha256 "5c1f244a618cfa131a80fbc16a2bc1672be738394a7980372fe572b0935ced0b"
 
   def install
     bin.install "nomadctl"
