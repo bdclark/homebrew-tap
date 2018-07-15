12bb2cda94c6cd83c40c96a391cc80d705f5d4d2
bdclark <brian@clark.zone>
2018-07-15 09:11:51 -0400

    consulssm version 0.0.3

--- Formula/consulssm.rb
+++ Formula/consulssm.rb
@@ -1,9 +1,9 @@
 class Consulssm < Formula
   desc ""
   homepage "https://github.com/bdclark/consulssm"
-  url "https://github.com/bdclark/consulssm/releases/download/0.0.2/consulssm_0.0.2_darwin_amd64.tar.gz"
-  version "0.0.2"
-  sha256 "924249b1c2b54c76d03f2ceb7554c1a13f6b488ff03f7f9d267b027a0ef3f013"
+  url "https://github.com/bdclark/consulssm/releases/download/0.0.3/consulssm_0.0.3_darwin_amd64.tar.gz"
+  version "0.0.3"
+  sha256 "707961bf85fd61830a02ea320eb5fb9b79ca80afcfea8a290d326b5aa0855790"
 
   def install
     bin.install "consulssm"
